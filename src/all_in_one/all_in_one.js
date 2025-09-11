ecfae67f27661c3a77bdb2e23e55b522f3a497ad
hiteshtechworks <144338235+hiteshtechworks@users.noreply.github.com>
2025-09-11 14:18:31 +0530

    Update all_in_one.js

--- src/all_in_one/all_in_one.js
+++ src/all_in_one/all_in_one.js
@@ -230,8 +230,6 @@ function delay(milliseconds) {
 
     console.log('Display Activity clicked');
 
-    console.log('All Tasks completed');
-
     return;
 
     // Wait for some action to complete (if necessary)
