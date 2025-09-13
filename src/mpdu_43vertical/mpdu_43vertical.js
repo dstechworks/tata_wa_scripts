bebf269cccf1555263beda6d4a25b1349eed439c
hiteshtechworks <144338235+hiteshtechworks@users.noreply.github.com>
2025-09-13 23:38:56 +0530

    deployment testing

--- src/mpdu_43vertical/mpdu_43vertical.js
+++ src/mpdu_43vertical/mpdu_43vertical.js
@@ -684,4 +684,8 @@ SHYD : ${verticalDataStoreArray[0].SHYD?.active || 0} (Active) / ${verticalDataS
     console.log("=====================================================================");
 }
 
+
+console.log("MPDU 43 VERTICAL SCRIPT RUNNING");
+console.log("MPDU 43 VERTICAL SCRIPT RUNNING");
+
 startScript();
