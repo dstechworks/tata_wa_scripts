804a6f0a3cff6e983d6cd5622cbb0b8093026727
hiteshtechworks <144338235+hiteshtechworks@users.noreply.github.com>
2025-11-07 18:42:13 +0530

    number added !!

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,7 @@ let mpduBranchWisePOCNum = {
     "NLUC": {
         "Vinay Jaiswal": "8377980245",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "WMUM": {
@@ -11,16 +12,20 @@ let mpduBranchWisePOCNum = {
         "Mohammed Amin Glasswala": "9930859493",
         "Akash Sagar": "8018508203",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "WAHM": {
         "Archish Chauhan": "9909036138",
+        "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "SHYD": {
         "Poulami Roy": "9948307149",
         "SURKANTI NIRANJAN": "9966332262",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "NJPR": {
@@ -30,93 +35,112 @@ let mpduBranchWisePOCNum = {
         "Sonu": "9784746786",
         "Mahesh": "9079737882",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "EVIZ": {
         "Harini Podagatlapalli": "6303239551",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "WBHO": {
         "Ashutosh Dwivedi": "9009912365",
         "Bharat Singh": "6267733463",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "WNAG": {
         "ANKUSH RATHOD": "9860068060",
         "KISHOR SAHARE": "8421196419",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "SBLR": {
         "Shreyas K": "8904657515",
         "Rohan": "9888311338",
         "Zaid": "8217625503",
+        "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915"
     },
     "SCHE": {
         "SANJAY K": "9842101922",
-        "Rohan": "9888311338",
         "Hariharasudhan": "9176049143",
+        "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915"
     },
     "WPUN": {
         "Kritika": "9816671018",
         "Danish Sayyed": "7020871947",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "EORI": {
         "Debasis Mohanty": "9078087356",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "ECAL": {
         "Rajdeep Ashoke Datta": "9657713025",
         "DIPANNITA TIWARY": "9836802859",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "EGAU": {
         "SUDIPTA RANJAN GOGOI": "7002973917",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "NSAH": {
         "Birjesh Gautam": "9716299579",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "NCHA": {
         "Vishal Bhardwaj": "9878425910",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915"
     },
     "NDEL": {
         "Amit Srivastava": "9792251777",
         "Devendra Gupta": "8506993808",
+        "Dilip Nadel": "9918001743",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "NEUP": {
         "Laksh Sethi": "9838347730",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "SKAR": {
         "Sudhir Shetty": "9901322557",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "SCOI": {
         "Gopinath R": "8610119246",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     },
     "SERN": {
         "Ravisankar K R": "9747260464",
         "Rohan": "9888311338",
+        "Narender Kumar": "9891493671",
         "Sandip": "9319798915",
     }
 };
