e6cddae02803f892804aa9e9e94392cb22cf5a4c
hiteshtechworks <144338235+hiteshtechworks@users.noreply.github.com>
2025-11-26 16:17:05 +0530

    cron set 3pm day time !!

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ cron.schedule('0 17 * * 1-6', () => {
 // Daily @ 03:00 PM: Run MPDU 43 Vertical
 ['0 15 * * *'].forEach(schedule => {
     cron.schedule(schedule, () => {
-        runScript('MPDU 43 Vertical', '~/tata_wa_scripts/src/mpdu_43vertical/mpdu_43vertical.js'); // 12 PM & 5 PM
+        runScript('MPDU 43 Vertical', '~/tata_wa_scripts/src/mpdu_43vertical/mpdu_43vertical.js');
     }, { timezone: TIMEZONE });
 });
 
